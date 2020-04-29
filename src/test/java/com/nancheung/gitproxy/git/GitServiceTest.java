699657911bd2f872011d6e16c8d29dc3e629a92c
NanCheung <nancheung97@gmail.com>
2020-04-30 01:28:06 +0800

    :white_check_mark: test(GitService): 新增两个单元测试

---
+++ src/test/java/com/nancheung/gitproxy/git/GitServiceTest.java
@@ -0,0 +1,33 @@
+package com.nancheung.gitproxy.git;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class GitServiceTest {
+    
+    public static final String GIT_URL = "https://github.com/nancheung97/Git-Proxy";
+    
+    @Autowired
+    private GitService gitService;
+    
+    @Test
+    void testClone() throws InterruptedException, ExecutionException, IOException {
+        File file = gitService.clone(GIT_URL);
+        System.out.println(file);
+    }
+    
+    @Test
+    void testGetGitInfo() {
+        GitInfo gitInfo = gitService.getGitInfo(GIT_URL);
+        System.out.println(gitInfo);
+    }
+}

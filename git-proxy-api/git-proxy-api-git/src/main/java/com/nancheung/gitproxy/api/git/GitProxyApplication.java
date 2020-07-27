c4285990b2aac8ba4790b60232fb9e416a975030
NanCheung <nancheung97@gmail.com>
2020-07-27 18:13:33 +0800

    :construction: feat(git-proxy-api-git): 写一个临时的UserDetailsService，以模拟授权校验

--- git-proxy-api/git-proxy-api-git/src/main/java/com/nancheung/gitproxy/api/git/GitProxyApplication.java
+++ git-proxy-api/git-proxy-api-git/src/main/java/com/nancheung/gitproxy/api/git/GitProxyApplication.java
@@ -1,11 +1,16 @@
 package com.nancheung.gitproxy.api.git;
 
 import com.nancheung.gitproxy.common.rbac.EnableRBAC;
+import com.nancheung.gitproxy.common.rbac.service.GitProxyUserDetailsService;
 import com.nancheung.gitproxy.common.swagger.EnableSwagger2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Git代理
@@ -19,8 +24,20 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 public class GitProxyApplication {
     
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
     public static void main(String[] args) {
         SpringApplication.run(GitProxyApplication.class, args);
+    }
+    
+    @Bean
+    public GitProxyUserDetailsService gitProxyUserDetailsService() {
+        return username -> User.builder()
+                .username(username)
+                .password(passwordEncoder.encode("{bcrypt}" + username))
+                .authorities(username + "|authorities")
+                .build();
     }
     
 }
